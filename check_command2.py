58f16eae3a82c2183363b0ab4f4ed78d0e4cb203
Radmir Shagiev <veselday@gmail.com>
2024-05-21 20:35:19 +0500

    прописан режим word

---
+++ check_command2.py
@@ -0,0 +1,17 @@
+import subprocess
+import string
+
+def check_command_output(command, text, mode='line'):
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    if mode == 'line':
+        if text in result.stdout:
+            return True
+        else:
+            return False
+    elif mode == 'word':
+        for word in result.stdout.split():
+            word_clean = ''.join(char for char in word if char not in string.punctuation)
+            if text == word_clean:
+                return True
+        return False
+    
